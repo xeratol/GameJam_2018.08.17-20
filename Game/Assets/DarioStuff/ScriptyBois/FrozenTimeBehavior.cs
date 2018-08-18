a36ce1e6cdf39b7c8ce86771c3a02a3c0f3f7cc3
daceblano <daceblano@gmail.com>
2018-08-18 16:21:24 -0700

    Some Tweaks
    - Added text to show current "time"
    - Modified where the lerping is done
    - Added limits on when the object is affected by "time"

--- Game/Assets/DarioStuff/ScriptyBois/FrozenTimeBehavior.cs
+++ Game/Assets/DarioStuff/ScriptyBois/FrozenTimeBehavior.cs
@@ -4,31 +4,22 @@ using UnityEngine;
 
 public class FrozenTimeBehavior : MonoBehaviour {
 
-    //lerping the time for smoothness :3
-    private float ObjectTime;
-    private float TargetTime;
-    private float alpha;
-    private float lerpspeed;
+    [SerializeField] private float ActionTimeFrom;
+    [SerializeField] private float ActionTimeTo;
 
     private Vector3 InitialPosition;
-
     [SerializeField] private Vector3 InitialVelocity;
     [SerializeField] private Vector3 InitialAcceleration;
 
     private ParticleSystem ParticleSys;
     private Animator Anim;
-    [SerializeField]private string AnimationState;
+    [SerializeField] private string AnimationState;
 
     // Use this for initialization
     void Start()
     {
         GameObject.Find("TimedObjectManager").GetComponent<TimedObjectManager>().RegisterObject(gameObject);
         InitialPosition = gameObject.GetComponent<Transform>().position;
-
-        TargetTime = 0f;
-        ObjectTime = 0f;
-        alpha = 1f;
-        lerpspeed = 0.5f;
 
         ParticleSys = gameObject.GetComponent<ParticleSystem>();
         if (ParticleSys)
@@ -47,27 +38,14 @@ public class FrozenTimeBehavior : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(!Mathf.Equals(alpha, 1f))
-        {
-            alpha += Time.deltaTime / lerpspeed;
-            if (alpha > 1f)
-            {
-                alpha = 1f;
-            }
-
-            ObjectTime = Mathf.Lerp(ObjectTime, TargetTime, alpha);
-            MoveWithTime(ObjectTime);
-        }
+        
 	}
 
-    public void SetTime (float CurrentTime)
+    public void MoveWithTime (float CurrentTime)
     {
-        TargetTime = CurrentTime;
-        alpha = 0f;
-    }
+        CurrentTime = Mathf.Clamp(CurrentTime, ActionTimeFrom, ActionTimeTo);
 
-    private void MoveWithTime (float CurrentTime)
-    {
+        CurrentTime -= ActionTimeFrom;
         GetComponent<Transform>().position = (InitialPosition) + (CurrentTime * InitialVelocity) + (.5f * CurrentTime * CurrentTime * InitialAcceleration);
 
         if (ParticleSys)
@@ -77,11 +55,8 @@ public class FrozenTimeBehavior : MonoBehaviour {
 
         if (Anim)
         {
-            //foreach (string s in AnimParamNames)
-            //{
             Anim.Play(AnimationState, -1, CurrentTime);
             Anim.speed = 0;
-            //}
         }
     }
 }
--- Game/Assets/DarioStuff/ScriptyBois/TimedObjectManager.cs
+++ Game/Assets/DarioStuff/ScriptyBois/TimedObjectManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimedObjectManager : MonoBehaviour {
 
@@ -8,36 +9,45 @@ public class TimedObjectManager : MonoBehaviour {
     private float CurrentTime;
     private float TimeStep;
 
-    private float TimeSpeed;
-    private float TimeFriction;
+    //lerping the time for smoothness :3
+    private float TargetTime;
+    private float alpha;
+    private float lerpspeed;
+
+    private Text Timer;
 
 	// Use this for initialization
 	void Awake () {
         TimedObjectList = new ArrayList();
-        CurrentTime = 0;
-        TimeStep = 1;
-        TimeSpeed = 0;
-        TimeFriction = 0.1f;
+
+        CurrentTime = 0f;
+        TimeStep = 1f;
+
+        TargetTime = 0f;
+        alpha = 1f;
+        lerpspeed = 0.5f;
+
+        Timer = GetComponentInChildren<Text>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //if (Mathf.Abs(TimeSpeed) < 1f / 2048f)
-        //{
-        //    return;
-        //}
+        if (!Mathf.Equals(alpha, 1f))
+        {
+            alpha += Time.deltaTime / lerpspeed;
+            if (alpha > 1f)
+            {
+                alpha = 1f;
+            }
 
-        //CurrentTime += TimeSpeed;
-        //if (CurrentTime < 0)
-        //{
-        //    CurrentTime = 0;
-        //}
-        
-        //foreach (GameObject g in TimedObjectList)
-        //{
-        //    g.GetComponent<FrozenTimeBehavior>().MoveTime(CurrentTime);
-        //}
-        //TimeSpeed -= TimeSpeed * TimeFriction;
+            CurrentTime = Mathf.Lerp(CurrentTime, TargetTime, alpha);
+            Timer.text = TimerTime(CurrentTime);
+
+            foreach (GameObject g in TimedObjectList)
+            {
+                g.GetComponent<FrozenTimeBehavior>().MoveWithTime(CurrentTime);
+            }
+        }
     }
 
     public void RegisterObject (GameObject TimedObject)
@@ -47,14 +57,18 @@ public class TimedObjectManager : MonoBehaviour {
 
     public void MoveTime (bool forward)
     {
-        //TimeSpeed += (forward ? .1f : -.1f) * TimeStep;
+        TargetTime += (forward ? .5f : -.5f) * TimeStep;
+        alpha = 0f; // interpolate
+    }
 
-        CurrentTime += (forward ? .5f : -.5f) * TimeStep;
-        CurrentTime = (CurrentTime < 0f ? 0f : CurrentTime);
+    private string TimerTime (float f)
+    {
+        bool nega = f < 0;
+        f *= nega ? -1f : 1f;
+        int minutes = (int)f / 60;
+        int seconds = (int)f - minutes * 60;
+        int milli = (int)(f * 100f) % 100;
 
-        foreach (GameObject g in TimedObjectList)
-        {
-            g.GetComponent<FrozenTimeBehavior>().SetTime(CurrentTime);
-        }
+        return ((nega ? "-" : "") + minutes.ToString("D1") + ":" + seconds.ToString("D2") + ":" + milli.ToString("D2"));
     }
 }
